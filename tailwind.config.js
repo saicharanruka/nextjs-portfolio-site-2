fbea1c417b8862770e7f514dad351c7a490be7da
slayer0x10 <saicharanruka@gmail.com>
2025-05-05 19:24:11 +0530

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./app/**/*.{js,ts,jsx,tsx}",
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				black: {
+					DEFAULT: "#000",
+					100: "#000319",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
